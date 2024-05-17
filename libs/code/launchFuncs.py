f585db429491af8654b9b270a33183d466ddcc40
Anthony Samartsev <ant.samarcev@gmail.com>
2024-05-17 18:36:22 +0300

    tables

--- libs/code/launchFuncs.py
+++ libs/code/launchFuncs.py
@@ -1,7 +1,7 @@
 from sys        import exit        as SYSEXIT
 from globalVars import launchTypes as LT
 from excelRW    import Excel
-from tables     import CellTable
+from tables     import CellTable, TableDict
 
 class launchScript():
     def __init__(self, book, type:str, log, errors):
@@ -10,11 +10,12 @@ class launchScript():
         self.log       = log       # потом заменим на класс Log
         self.errors    = errors    # потом заменим на класс Errors
         self.fullRange = LT[type]['fullRange']
-        self.toTD      = LT[type]['toTD']
+        self.toTD      = LT[type]['toTD']   # будем работать с TableDict (true) или же с CellTable (false)
 
         # создаём объекты для последующей обработки
-        self.file      = Excel(book, self.fullRange)
-        self.table     = True if self.toTD else CellTable(self.file.table.data) # потом заменить первый True
+        self.file                = Excel(book, self.fullRange)
+        if self.toTD: self.table = TableDict  (self.file.table)
+        else:         self.table = CellTable  (self.file.table)
 
 # защита от запуска модуля
 if __name__ == '__main__':
--- libs/code/tables.py
+++ libs/code/tables.py
@@ -1,19 +1,43 @@
-from sys import exit as SYSEXIT
+from sys  import exit as SYSEXIT
+from copy import deepcopy
 
-class Table():  # общий класс, можно копировать без изменений в другие программы
-    def __init__(self, values:list):    # values – таблица [[]]
+def getCells_fromList(values:list, errors=False):   # errors – значение по умолчанию для всех ячеек
+    return [{'value':val, 'error':errors} for val in values]
+
+# надкласс с общими функциями для Table() и CellTable()
+class TableTemplate():
+    def rotate(self, saveRotated=False):
+        # если saveRotated=True, сохранить результат в self.data, иначе только вернуть результат в return
+        rotated = [[] for cell in self.data]
+        for r in range(len(self.data)):
+            for c in range(len(self.data[r])):
+                rotated[c].append(self.data[r][c])
+
+        if saveRotated: self.data = rotated
+        return deepcopy(rotated)    # deepcopy на случай, если в ячейках таблицы другие объекты
+
+# основные классы
+class Table(TableTemplate):             # общий класс, можно копировать без изменений в другие программы
+    def __init__(self, values:list):    # values – таблица[[]]
         self.data = values
-        self.size = {'rows': len(values), 'cols': len(values[0])}
-
-class CellTable():
+    def rotate(self, saveRotated=False):
+        return Table(super().rotate(saveRotated))
+class CellTable(TableTemplate):
     # общий класс, ВОЗМОЖНО подойдёт для других программ
     # это таблица, в которой каждая ячейка – это словарь [[{value:'', error:true/false}, ...], ...]
-    def __init__(self, values:list, errors=False):  # values – таблица [[]], errors – значение по умолчанию для всех ячеек
-        self.data = []
-        for row in values:
-            self.data.append([])
-            for cell in row:
-                self.data[-1].append({'value':cell, 'error':errors})
+    def __init__(self, table:Table, errors=False):  # errors – значение по умолчанию для всех ячеек
+        self.data = [getCells_fromList(row, errors) for row in table.data]
+    def rotate(self, saveRotated=False):
+        # алгоритм выстроен так, чтобы не терять значения errors
+        new      = CellTable(Table([[]]))
+        new.data = super().rotate(saveRotated)
+        return new
+class TableDict():
+    # общий класс, ВОЗМОЖНО подойдёт для других программ
+    # это таблица, представленная в виде словаря {столбец: {title:CellObj, cells:[CellObj,...]},...}
+    def __init__(self, table:Table, errors=False):  # errors – значение по умолчанию для всех ячеек
+        rotated = table.rotate()
+
 
 # защита от запуска модуля
 if __name__ == '__main__':
