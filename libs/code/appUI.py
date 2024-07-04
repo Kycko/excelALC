3c4255bc2fae3560cc83e5f0f77d454b685b0679
Anthony Samartsev <ant.samarcev@gmail.com>
2024-07-04 13:36:49 +0300

    finished core window (w/o widgets)

--- libs/code/appUI.py
+++ libs/code/appUI.py
@@ -5,9 +5,11 @@ import globalVars      as G
 # окно программы
 class Window(TBS.Window):
     def __init__(self):
-        super().__init__(title=G.app['TV'], themename=G.app['themes'][G.config.get('main:darkTheme')])
-        self.setGeometry()
-        self.buildFrames()
+        super().__init__(title     = G.app['TV'],
+                         themename = G.app['themes'][G.config.get('main:darkTheme')],
+                         size      = G.app['size'],
+                         minsize   = G.app['size'])
+        self.place_window_center()  # расположить в центре экрана
 
 # защита от запуска модуля
 if __name__ == '__main__':
--- libs/code/rootClass.py
+++ libs/code/rootClass.py
@@ -1,9 +1,10 @@
-from sys import exit as SYSEXIT
-
+from sys   import exit as SYSEXIT
+from appUI import Window
 # корневой класс: из него запускаются UI и код других модулей
 class Root():
     def __init__(self):
-        
+        self.UI = Window()
+        self.UI.mainloop()
 
 # защита от запуска модуля
 if __name__ == '__main__':
