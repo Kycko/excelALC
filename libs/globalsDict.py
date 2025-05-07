30b7c4d8060d12164226310731e92433cca51b20
Anthony Samartsev <ant.samarcev@gmail.com>
2025-05-08 01:39:45 +0300

    +confirmWrite option

--- libs/globalsDict.py
+++ libs/globalsDict.py
@@ -1,30 +1,25 @@
-from sys import exit as SYSEXIT
+from   sys import exit as SYSEXIT
+import dictFuncs       as dictF
 
 class globDicts():  # импортируется в G.dict (в глобальные переменные)
   def __init__(self):
     # эти свойства записываются в rootClasses.pr{} (properties)
-    self.tasks = {'chkCat':{'read'        : 'selection',
-                            'toTD'        :  False,
-                            'addHeader'   :  False,
-                            'launch'      : 'rangeChecker',
-                            'justVerify'  :  False,
-                            'resetBg'     : 'selection',
-                            'hlTitles'    :  False,
-                            'AStype'      : 'cat',
-                            'cfg'         :('newSheet',
-                                            'suggErrors',
-                                            'saveAfter')},
-                  'chkSrc':{'read'        : 'selection',
-                            'toTD'        :  False,
-                            'addHeader'   :  False,
-                            'launch'      : 'rangeChecker',
-                            'justVerify'  :  False,
-                            'resetBg'     : 'selection',
-                            'hlTitles'    :  False,
-                            'AStype'      : 'source',
-                            'cfg'         :('newSheet',
-                                            'suggErrors',
-                                            'saveAfter')}}
+    def _getShared(type:str,upd={}):
+      # функция, передающая в self.tasks одинаковые свойства для однотипных элементов
+      if type == 'cfg': return ['newSheet','suggErrors','confirmWrite','saveAfter']
+      else:
+        dict = {'chk':{'cfg'       :_getShared('cfg'),
+                       'read'      :'selection',
+                       'toTD'      : False,
+                       'addHeader' : False,
+                       'launch'    :'rangeChecker',
+                       'justVerify': False,
+                       'resetBg'   :'selection',
+                       'hlTitles'  : False}}
+        return dictF.update(dict[type],**upd)
+
+    self.tasks = {'chkCat':_getShared('chk',{'root':{'AStype':'cat'}}),
+                  'chkSrc':_getShared('chk',{'root':{'AStype':'source'}})}
 
 # защита от запуска модуля
 if __name__ == '__main__':
